b1ed625c03ddc2e2ae73aa933c6ff2c49cfaa02a
ethannschwartz <96845694+ethannschwartz@users.noreply.github.com>
2024-08-07 13:00:29 +0300

    updates to ensure performance

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,20 +1,22 @@
 <template>
-  <header data-aos="fade-down" class="sticky top-0 p-6 bg-teal-50 dark:bg-teal-950 flex items-center justify-between">
+  <header data-aos="fade-down" class="z-20 sticky top-0 p-6 bg-teal-50 dark:bg-teal-950 flex items-center justify-between">
     <h1 class="flex items-center gap-1 font-bold type-primary">
       <img class="w-5 mx-auto block dark:hidden" src="https://nuxtjs.ir/logos/nuxt-square.svg" alt="">
       <img class="w-5 mx-auto hidden dark:block" src="https://nuxtjs.ir/logos/nuxt-square-white.svg" alt="">
       Nuxt Template
     </h1>
-    <UTooltip :text="`Turn dark mode ${useState('isDarkMode').value ? 'off':'on'}`">
+    <UTooltip :text="`Turn dark mode ${isDarkMode ? 'off':'on'}`">
       <button @click="toggleDarkMode">
-        <i :class="useState('isDarkMode').value ? 'fi-rr-sun text-white':'fi-rr-moon'" class="fi"></i>
+        <i :class="isDarkMode ? 'fi-rr-sun text-white':'fi-rr-moon'" class="fi"></i>
       </button>
     </UTooltip>
   </header>
 </template>
 
 <script setup>
+const isDarkMode = useState("isDarkMode");
+
 const toggleDarkMode = () => {
-  useState("isDarkMode").value = !useState("isDarkMode").value;
+  isDarkMode.value = !isDarkMode.value;
 }
 </script>
