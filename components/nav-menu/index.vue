064bc72d5db9ec40cd51ac8423b011505bdf912e
ethanschwartz <e.schwartz@seclock.com>
2025-10-28 13:01:47 +0200

    - added menu for mobile

--- components/nav-menu/index.vue
+++ components/nav-menu/index.vue
@@ -1,13 +1,11 @@
 <template>
-  <ul class="w-full flex flex-col items-center justify-end gap-3">
-    <li v-for="link in sections" :key="link.href" class="w-full !divide-y divide-gray-400">
-      <UTooltip :text="link.tooltip" :delay-duration="1000">
-        <a :href="link.href" class="active:bg-gray-100 p-1 flex w-full justify-end duration-300">
-          <div class="text-lg font-medium text-right w-fit relative">
-            {{link.name}}
-          </div>
-        </a>
-      </UTooltip>
+  <ul class="w-full flex flex-col items-center justify-end">
+    <li v-for="link in sections" :key="link.href" class="w-full">
+      <a :href="link.href" class="active:bg-gray-100 p-2 flex items-center w-full justify-end duration-300">
+        <div class="text-lg font-semibold text-right w-fit relative">
+          {{link.name}}
+        </div>
+      </a>
     </li>
   </ul>
 </template>
